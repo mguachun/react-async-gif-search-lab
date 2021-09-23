c5ce557d3183d360a341d83af78283d483c3568e
Melissa Guachun <1212malka@gmail.com>
2021-09-23 13:29:24 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-
-import NavBar from './NavBar'
+import GifListContainer from "../containers/GifListContainer";
+import NavBar from './NavBar';
 
 // the App component should render out the GifListContainer component 
 
@@ -8,8 +8,9 @@ const App = () => {
   return (
     <div>
         < NavBar color='black' title="Giphy Search" />
+        <GifListContainer />
     </div>
-  )
-}
+  );
+};
 
 export default App
---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import GifList from '.../components/GifList'
+import GifSearch from '.../components/GifSearch'
+
+class GifListContainer extends Component {
+    state = {
+        gifs: []
+    }
+
+    render() {
+        return(
+            <div>
+                <GifSearch fetchGIFS={this.fetchGIFS} />
+                <GifList gifs={this.state.gifs} />
+            </div>
+        )
+    }
+
+    fetchGIFS = (query = "dolphins") => {
+        fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`)
+        .then(res => res.json())
+        .then(({data}) => {
+            this.setState({ gifs: data.map( gif => ({ url: gif.images.original.url }) ) })
+        })
+    }
+
+    componentDidMount() {
+        this.fetchGIFS()
+
+    }
+
+}
+
+export default GifListContainer
